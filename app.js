4f327a56c4a920f7d638a4d9db849a33cb46b725
Morde Yoiris <morde1515@gmail.com>
2020-12-08 18:51:45 +0200

    updated

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-const { json } = require("body-parser");
 
 const app = express();
 
