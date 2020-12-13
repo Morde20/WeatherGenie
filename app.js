9f4cc86d31495195de119d0ec85410cb88f66185
Morde Yoiris <morde1515@gmail.com>
2020-12-13 19:33:53 +0200

    Added a procfile

--- app.js
+++ app.js
@@ -67,6 +67,4 @@ app.post("/newcity", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000.");
-});
+app.listen(process.env.PORT);
